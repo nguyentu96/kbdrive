0da0c5b745c510ba2d2315628904aac18f9fa092
nguyentu96 <ngoctu.tu1@gmail.com>
2024-10-06 14:31:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-console.log("v3");
+console.log("v4");
 
 // GitHub API Variables
-const token = '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'; // Replace with your GitHub PAT
+const token = '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'; // Replace with your GitHub PAT
 const owner = 'nguyentu96'; // Your GitHub username or organization
 const repo = 'kbdrive'; // Your repository name
 const path = 'uploaded-files'; // Path where the file will be uploaded
